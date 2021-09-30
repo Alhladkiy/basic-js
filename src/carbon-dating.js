cbd9b1e0246aab2205059032f110506bf3818d75
Alhladkiy <Alhladkiy@gmail.com>
2021-09-30 21:48:40 +0300

    add carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,20 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+export default function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string' || sampleActivity.trim().length === 0  || isNaN(Number(sampleActivity))) {
+    return false
+  }
+
+  const k  = Math.log(Number(2)) / HALF_LIFE_PERIOD;
+  
+  var a = (Math.log(MODERN_ACTIVITY / sampleActivity)) / k
   // remove line with error and write your code here
+  const result = Math.ceil(a);
+  
+  if ((isNaN(result) || result < 0 || !Number.isFinite(result))) {
+    return false;
+  }
+  return result;
 }
+console.log(dateSample('-55.8'))
--- src/st-delete-digit.js
+++ src/st-delete-digit.js
@@ -1,5 +1,3 @@
-import { NotImplementedError } from '../extensions/index.js';
-
 /**
  * Given some integer, find the maximal number you can obtain
  * by deleting exactly one digit of the given number.
